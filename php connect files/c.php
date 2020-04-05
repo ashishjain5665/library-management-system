c9bfd406041abb8fc12330af7322c7404ed59f4d
Ashish Jain <52023930+ashishjain5665@users.noreply.github.com>
2020-04-05 15:52:24 +0530

    Add files via upload

---
+++ php connect files/c.php
@@ -0,0 +1,26 @@
+<?php 
+$i=$_POST['ii'];
+$aa=$_POST['a'];
+$bb=$_POST['b'];
+if($aa != $bb){
+	echo"please fill the entries carefully";
+	header("location:student_year_update.php");
+}
+else{
+	mysql_connect("localhost","root","");
+	
+	mysql_select_db("library management system");
+	$query = "SELECT * FROM student WHERE stu_id = '$i'";
+	$r=mysql_query($query);
+	$c =mysql_num_rows($r);
+	if($c ==0){
+		echo"not exists";
+	}
+	else{
+	$query1 = "UPDATE student SET stu_year ='$aa' where stu_id='$i'";
+	mysql_query($query1);
+	echo"done";
+	} 
+}
+
+?>
