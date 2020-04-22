1695dd66a8e3758cb134c06785ef0d4977ec2098
Ashish Jain <52023930+ashishjain5665@users.noreply.github.com>
2020-04-22 21:51:32 +0530

    Add files via upload

---
+++ php connect files/z.php
@@ -0,0 +1,24 @@
+<?php
+$aa=$_POST['a'];
+$bb=$_POST['b'];
+$cc=$_POST['c'];
+if($aa == "" || $bb == "" ||$cc==""){
+	echo"fill all details";
+}
+elseif($cc != $bb){
+	echo "fill the entries carefully";
+}
+else{
+	mysql_connect("localhost","root","");
+	mysql_select_db("library management system");
+	$query = "SELECT * FROM book WHERE book_id='$aa'";
+	$r = mysql_query($query);
+	$c = mysql_num_rows($r);
+
+	if($c > 0){
+			$query1 = "UPDATE book SET no_of_book_available='$bb' WHERE book_id='$aa'";
+			mysql_query($query1);
+			echo"done";
+			}
+}
+?>
