cd6d5747a9a83b14ccd26a102de4dbd3614ca9c1
Ashish Jain <52023930+ashishjain5665@users.noreply.github.com>
2020-04-24 22:37:54 +0530

    Create add_book.php

---
+++ php forms files/add_book.php
@@ -0,0 +1,29 @@
+<html>
+<style>
+.vir
+{
+
+width:800px;
+text-decoration:underline;
+font-size:120px;
+color:#fff;		
+margin-left: 500px;
+margin-bottom: 0px;
+font-family:Brush Script Std;
+}
+
+</style>
+<body background="web.jpg" style="margin:0px; padding:0px; position:fixed; margin:0px; opacity:0.9; z-index:-1; height:100%; width:100%">
+<p class="vir" >Add_new_book</p>
+<body background="web.jpg" style="margin:0px; padding:0px; position:fixed; margin:0px; opacity:0.9; z-index:-1; height:100%; width:100%">
+	<form method="POST" action="add_book_connect.php">
+		<table cellpadding="30" align="center">
+			<tr><td><font size = "80">Enter book id</font></td> <td>:</td> <td><input type="number" name="a"></td></tr>
+			<tr><td><font size = "80">Enter Book name</font></td> <td>:</td> <td><input type="text" name="b"></td></tr>
+			<tr><td><font size = "80">Enter author name</font></td> <td>:</td> <td><input type="text" name="c"></td></tr>
+			<tr><td><font size = "80">Enter number of books available</font></td> <td>:</td> <td><input type="number" name="d"></td></tr>
+			<tr><td><input type="submit" value="add"></td></tr>
+	</form>
+</table></form>
+</body>
+</html>
