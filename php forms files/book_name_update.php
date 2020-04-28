8a8376d17ce368cbb746e5a7f8257367183a6ccf
Ashish Jain <52023930+ashishjain5665@users.noreply.github.com>
2020-04-28 22:46:44 +0530

    Add files via upload

---
+++ php forms files/book_name_update.php
@@ -0,0 +1,27 @@
+<html>
+<style>
+.vir
+{
+
+width:800px;
+text-decoration:underline;
+font-size:120px;
+color:#fff;		
+margin-left: 300px;
+margin-bottom: 0px;
+font-family:Brush Script Std;
+}
+
+</style>
+<body background="web.jpg" style="margin:0px; padding:0px; position:fixed; margin:0px; opacity:0.9; z-index:-1; height:100%; width:100%">
+<p class="vir" >update_book_name</p>
+<form method="POST" action="x.php">
+	<table cellpadding="20" align="center">
+		<tr><td><font size="50">Enter the book id</td></font> <td>:</td> <td><input type="number" name="a"></td></tr>
+		<tr><td><font size="50">Enter the new name</td></font> <td>:</td> <td><input type="text" name="b"></td></tr>
+		<tr><td><font size="50">rewrite the book name</td></font> <td>:</td> <td><input type="text" name="c"></td></tr>
+		<tr><td><input type="submit" value="update" size="50"></td></tr>
+	</table>
+</form>
+</body>
+</html>
