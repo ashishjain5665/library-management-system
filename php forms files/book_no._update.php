d234b2cd6e366f0106e9d1b129084039746f232a
Ashish Jain <52023930+ashishjain5665@users.noreply.github.com>
2020-04-30 15:01:56 +0530

    Add files via upload

---
+++ php forms files/book_no._update.php
@@ -0,0 +1,26 @@
+<html>
+<style>
+.vir
+{
+
+width:800px;
+text-decoration:underline;
+font-size:120px;
+color:#fff;		
+margin-left: 00px;
+margin-bottom: 0px;
+font-family:Brush Script Std;
+}
+
+</style>
+<body background="web.jpg" style="margin:0px; padding:0px; position:fixed; margin:0px; opacity:0.9; z-index:-1; height:100%; width:100%">
+<p class="vir" >update_number_of_books_available</p>
+<form method="POST" action="z.php">
+	<table cellpadding="30" align="center">
+		<tr><td><font size = "50">Enter the book id</td> <td>:</td> <td><input type="number" name="a"></td></tr>
+		<tr><td><font size = "50">Enter the number of books available</td> <td>:</td> <td><input type="number" name="b"></td></tr>
+		<tr><td><font size = "50">rewrite the number of books available</td> <td>:</td> <td><input type="number" name="c"></td></tr>
+		<tr><td><input type="submit" value="update"></td></tr>
+	</table></font>
+</form>
+</html>
