2a6cc1537ee0c109bfd0572aa6058036e6768b8f
Ashish Jain <52023930+ashishjain5665@users.noreply.github.com>
2020-05-02 12:31:25 +0530

    Add files via upload

---
+++ php forms files/del_student.php
@@ -0,0 +1,24 @@
+<html>
+<style>
+.vir
+{
+
+width:800px;
+text-decoration:underline;
+font-size:120px;
+color:#fff;		
+margin-left: 300px;
+margin-bottom: 0px;
+font-family:Brush Script Std;
+}
+
+</style>
+<body background="web.jpg" style="margin:0px; padding:0px; position:fixed; margin:0px; opacity:0.9; z-index:-1; height:100%; width:100%">
+<p class="vir" >Delete student</p>
+	<form method="POST" action="del_student_connect.php">
+		<table cellpadding="30" align="center">
+			<tr>	<td><font size="50">Enter student id</td>	<td>:</td>	 <td><input type="number" name="a" /></td>	</tr>
+			<tr><td><input type="submit" value="delete" /></td></tr>
+	</form> 
+</html>
+
