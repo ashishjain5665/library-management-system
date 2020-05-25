cad80d502bcbbd093bcf1a553d71c430f4307436
Ashish Jain <52023930+ashishjain5665@users.noreply.github.com>
2020-05-25 14:11:30 +0530

    Add files via upload

---
+++ php forms files/login_student.php
@@ -0,0 +1,27 @@
+<html>
+<style>
+.vir
+{
+
+width:800px;
+text-decoration:underline;
+font-size:120px;
+color:#fff;		
+margin-left: 550px;
+margin-bottom: 0px;
+font-family:Brush Script Std;
+}
+
+</style>
+<body background="web.jpg" style="margin:0px; padding:0px; position:fixed; margin:0px; opacity:0.9; z-index:-1; height:100%; width:100%">
+<p class="vir" >Login Student</p>
+
+<form name="f1" action="login_student_connect.php" method="POST">
+	<table cellpadding="30" align="center">
+	<tr><td><font size="50">Enter your user id</td> <td>:</td> <td><input type="number" name="a"></td></tr>
+	<tr><td><font size="50">Enter your password</font></td> <td>:</td> <td><input type="password" name="b"></td></tr>
+	<tr><td colspan="3" align="center"><input type="submit" value="login"></td></tr>
+	<tr><td colspan="3" align="center"><input type="reset" value="reset"></td></tr>
+</table>
+</form>
+</html>
